c7f60e541779274744914e3cbf053181d72ce7c9
Weslley Milani <weslley.milani@icloud.com>
2025-06-10 18:17:45 -0300

    Day1 challenge

---
+++ Day1/day1.py
@@ -0,0 +1,7 @@
+#Day 1: Trata-se de um exercício bem simples no qual, devemos imprimir na tela algum texto proposto pelo professor.
+
+print('Milani!\n'
+      '10/06/2025\n'
+      'Estou entrando no desafio de 100 dias de Python do Replit!\n'
+      'Para esse desafio não precisamos mais que alguns minutos do nosso dia para colocar em prática o que aprendemos.\n'
+      'Espero que eu consiga concluir o desafio e aprender mais sobre Python!\n')
