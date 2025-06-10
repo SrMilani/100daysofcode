229a6048cb06ce608b7b511ee5792240e9052624
Weslley Milani <weslley.milani@icloud.com>
2025-06-10 18:46:44 -0300

    Day 2 challenge

---
+++ Day2/day2.py
@@ -0,0 +1,10 @@
+print('Que bom ver você por aqui!\n')
+name = input('Qual é o seu nome? ')
+idade = int(input('Quantos anos você tem? '))
+comida_favorita = input('Qual é a sua comida favorita? ')
+genero_musical = input('Qual é o seu gênero musical favorito? ')
+onde_mora = input('Onde você mora? ')
+print('Olá, ' + name + '! É um prazer te conhecer!\n'
+                       'Provavelmnete você está com fome e gostaria de comer uma ' + comida_favorita + ', o que acha?\n'
+      'Seria interessante ouvir um(a) ' + genero_musical + ' enquanto come, não acha?\n'
+      'Isso tudo seria ainda melhor se você estivesse em ' + onde_mora + ', sua cidade natal!\n')
