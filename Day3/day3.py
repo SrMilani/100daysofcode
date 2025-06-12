1073ccbd7c205f8317fb9b5c50eb079011b3cddb
Weslley Milani <weslley.milani@icloud.com>
2025-06-11 22:25:45 -0300

    Day3 Challenge

---
+++ Day3/day3.py
@@ -0,0 +1,6 @@
+# Criação das variáveis a serem concatenadas
+name_person = input('Digite o seu nome: ')
+nome_comida = input('Digite o nome da comida: ')
+nome_de_tempero = input('Digite o nome de um tempero: ')
+
+print('Olá, ' + name_person + '!', 'Ótimo você cozinhou! ' + nome_comida + ' com ' + nome_de_tempero + '!')
